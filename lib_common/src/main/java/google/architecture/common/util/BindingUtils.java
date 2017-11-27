ec64b98d8f51ac842655268ebd5aaa69864b84f1
dawish <742637254@qq.com>
2017-11-27 11:04:26 +0800

    # :wrench: 升级BindingUtils支持更多的DataBinding类型

--- lib_common/src/main/java/google/architecture/common/util/BindingUtils.java
+++ lib_common/src/main/java/google/architecture/common/util/BindingUtils.java
@@ -5,6 +5,7 @@ import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.FragmentStatePagerAdapter;
 import android.support.v4.view.ViewPager;
 
+import android.view.View;
 import com.facebook.drawee.view.SimpleDraweeView;
 
 /**
@@ -19,13 +20,28 @@ public class BindingUtils {
             image.setImageURI(uri);
         }
     }
+
+    @BindingAdapter("bind:image")
+    public static void loadImage(SimpleDraweeView image, int id){
+        if(image!=null){
+            image.setImageResource(id);
+        }
+    }
+
     @BindingAdapter("bind:onNavigationItemSelectedListener")
     public static void setOnNavigationItemSelectedListener(
             BottomNavigationView view, BottomNavigationView.OnNavigationItemSelectedListener listener) {
         view.setOnNavigationItemSelectedListener(listener);
     }
+
     @BindingAdapter("bind:viewPagerAdapter")
     public static void setViewPagerAdapter(ViewPager view, FragmentStatePagerAdapter adapter) {
         view.setAdapter(adapter);
     }
+
+    @BindingAdapter("visibleGone")
+    public static void showHide(View view, boolean show) {
+        view.setVisibility(show ? View.VISIBLE : View.GONE);
+    }
+
 }
